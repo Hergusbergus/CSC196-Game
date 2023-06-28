c7c140005012fa81fc786cb06a3ba878e50c3143
Hergusbergus <zane.ammerman@gmail.com>
2023-06-27 18:43:07 -0600

    New update for homework

---
+++ Source/Engine/Core/FileIO.cpp
@@ -0,0 +1,8 @@
+#include "FileIO.h"
+namespace kiko
+{
+	std::string kiko::getFilePath()
+	{
+		return std::filesystem::current_path().string();
+	}
+}
--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include "Core/Random.h"
+#include "Core/FileIO.h"
+using namespace std;
+using namespace kiko;
 int main()
 {
-#ifdef _DEBUG
-	std::cout << "Debug!" << std::endl;
-#endif
-	std::cout << "Hello World!" << std::endl;
+	cout << getFilePath() << endl;
+	seedRandom((unsigned int)time(nullptr));
+	for (int i = 0; i < 3; i++)
+	{
+		cout << random(5, 10) << endl;
+	}
 }
