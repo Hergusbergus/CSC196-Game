8799da92133b71d3ec77fba173fd5eac73128594
Hergusbergus <zane.ammerman@gmail.com>
2023-07-03 20:41:13 -0600

    Adding Property Sheets.

---
+++ Source/Engine/Core/Memory.cpp
@@ -0,0 +1,32 @@
+#include "Memory.h"
+#include <iostream>
+kiko::MemoryTracker kiko::g_memoryTracker;
+void* operator new (size_t size)
+{
+	void* p = malloc(size);
+	kiko::g_memoryTracker.Add(p, size);
+	return p;
+}
+void operator delete (void* address, size_t size)
+{
+	kiko::g_memoryTracker.Remove(address, size);
+	free(address);
+}
+namespace kiko
+{
+	void MemoryTracker::Add(void* address, size_t size)
+	{
+		m_bytesAllocated += size;
+		m_numAllocations++;
+	}
+	void MemoryTracker::Remove(void* address, size_t size)
+	{
+		m_bytesAllocated -= size;
+		m_numAllocations--;
+	}
+	void MemoryTracker::DisplayInfo()
+	{
+		std::cout << "Current bytes allocated: " << m_bytesAllocated << std::endl;
+		std::cout << "Current number of allocations: " << m_numAllocations << std::endl;
+	}
+}
--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
 #include "Core/Random.h"
 #include "Core/FileIO.h"
+#include "Core/Memory.h"
+#include "Core/Time.h"
+#include "Renderer/Renderer.h"
 using namespace std;
 using namespace kiko;
+using namespace chrono;
 int main()
 {
-	cout << getFilePath() << endl;
-	seedRandom((unsigned int)time(nullptr));
-	for (int i = 0; i < 3; i++)
+	g_memoryTracker.DisplayInfo();
+	int* p = new int;
+	g_memoryTracker.DisplayInfo();
+	delete p;
+	g_memoryTracker.DisplayInfo();
+	Time timer;
+	for (int i = 0; i < 1000000; i++)
 	{
-		cout << random(5, 10) << endl;
+
 	}
+	cout << timer.GetElapsedMilliseconds() << endl;
+	//auto i = true;
+	//auto start = high_resolution_clock::now;
+	//for (int i = 0; i < 100000; i++)
+	//{
+
+	//}
+	//auto end = high_resolution_clock::now();
+	//cout << duration_cast<nanoseconds> (end - start).count() << endl;
+	//cout << getFilePath() << endl;
+	//setFilePath("Assets");
+	//cout << getFilePath() << endl;
+	//size_t size;
+	//getFileSize("Text.txt", size);
+	//cout << size << endl;
+	//string s;
+	//readFile("Text.txt", s);
+	//cout << s << endl;
+	//seedRandom((unsigned int)time(nullptr));
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	cout << random(5, 10) << endl;
+	//}
 }
